e71aa02ffad57fffcab69eaa45b73cfab4de12b7
AlanTai <rainman.tai@gmail.com>
2015-10-03 13:35:55 -0700

    update dict

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,27 @@ var router = express.Router();
 
 // get page information
 var jsonfile = require('jsonfile');
+var index_info;
 jsonfile.readFile('./my_dicts/index.json', function(err, obj) {
+  index_info = obj;
   console.log(obj);
 })
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index-demo', { title: 'Express' });
+  var lang;
+  if( req.query.hasOwnProperty('lang') ){
+    lang = req.query.lang;
+  }else{
+    lang = 'english';
+  }
+
+  if( !index_info.hasOwnProperty(lang) ){
+    lang = 'english';
+  }
+
+  console.log(index_info[lang]['title']);
+  res.render('index-demo', { title : index_info[lang]['title'] });
 });
 
 module.exports = router;
