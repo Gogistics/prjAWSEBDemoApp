38b47f628668453a28fe11244700b2dfaba25534
AlanTai <rainman.tai@gmail.com>
2015-10-09 15:35:25 -0700

    add new wav files

---
+++ music_demo/timbre/misc/audio/fairlyconfusing_drum_by_mouth/readme.txt
@@ -0,0 +1,9 @@
+Drum by mouth.
+
+This is another unusual percussion sample set I made. This time, it is made from sounds of mouth. I took a session of myself making strange noises in front of microphone and then cut the sounds to make them drum alike. I used slower playback for some of the sounds to lower the pitch, which is something I enjoy doing with all kinds of sample recordings. Lowering the pitch was helpful in making some samples sound closer to bass drum, as I think my mouth is not big enough to emit a proper kick sound. Original recording was 88.2 kHz, some sounds has been then set to playback at 44.1 kHz (samples named 'slow' and some 'kick' sound) and some at 22 kHz (named 'double slow' and some of 'kicks'). In the end, all sounds have been re-sampled to 44.1 kHz. I used no effects, besides mentioned manipulation of playback speed. Hardware I used is Audio Technica AT3035 microphone, Focusrite ISO One preamp and Echo Audiofire card. Samples were edited in Wavosaur and re-sampled with SOX.
+
+The archive should contain 222 samples, wav format, mono 44.1 kHz / 24 bit. I included Battery 2 kit I assembled for demo track, made from selection of 40+ samples. I suggest you to make your own  mapping for your sampler, one included is just a hint. 
+
+-----------------------------------
+contact: marcin@fairlyconfusing.net
+www.fairlyconfusing.net
--- public/javascripts/music_demo_beatbox.js
+++ public/javascripts/music_demo_beatbox.js
@@ -1,9 +1,8 @@
-/* beatbox */
 T("audio").load("/timbre.js/misc/audio/drumkit.wav", function() {
-  var BD  = this.slice(   0,  500).set({bang:false});
-  var SD  = this.slice( 500, 1000).set({bang:false});
-  var HH1 = this.slice(1000, 1800).set({bang:false, mul:0.2});
-  var HH2 = this.slice(1000, 2200).set({bang:false, mul:0.5});
+  var BD  = this.slice(   0,  800).set({bang:false});
+  var SD  = this.slice( 800, 1000).set({bang:false});
+  var HH1 = this.slice(1000, 1500).set({bang:true, mul:0.4});
+  var HH2 = this.slice(1500, 2000).set({bang:false, mul:0.2});
   var CYM = this.slice(2000).set({bang:false, mul:0.2});
   var scale = new sc.Scale([0,1,3,7,8], 12, "Pelog");
 
@@ -18,29 +17,33 @@ T("audio").load("/timbre.js/misc/audio/drumkit.wav", function() {
     [SD],
   ].wrapExtend(128);
 
-  var P2 = sc.series(16);
+  var P2 = sc.series(18);
 
-  var drum = T("lowshelf", {freq:300, gain:3, mul:0.8}, BD, SD, HH1, HH2, CYM).play();
+  var drum = T("lowshelf", {freq:110, gain:8, mul:0.6}, BD, SD, HH1, HH2, CYM).play();
   var lead = T("saw", {freq:T("param")});
-  var vcf  = T("MoogFF", {freq:250, gain:2, mul:0.9}, lead);
-  var env  = T("perc", {r:300});
-  var arp  = T("OscGen", {wave:"sin(10)", env:env, mul:0.5});
-  var msec  = timbre.timevalue("bpm120 l8");
-  var synth = T("OscGen", {env:T("perc", {r:msec, ar:true})});
+  var vcf  = T("MoogFF", {freq:2400, gain:6, mul:0.1}, lead);
+  var env  = T("perc", {r:100});
+  var arp  = T("OscGen", {wave:"sin(17)", env:env, mul:0.5});
 
-  T("delay", {time:"BPM128 L4", fb:0.65, mix:0.25}, 
-    T("pan", {pos:0.2}, vcf), 
-    T("pan", {pos:T("tri", {freq:"BPM64 L1", mul:0.8}).kr()}, arp)
+  //
+  var table = [200, [4800, 150], [2400, 500]];
+  var cutoff = T("sin", {freq:"400ms", mul:800, add:1760}).kr();
+  var VCO = T("saw", {gain: 1, mul:0.2});
+
+  T("delay", {time:"BPM128 L4", fb:0.65, mix:0.35}, 
+    T("pan", {pos:0.6}, vcf), 
+    T("pan", {pos:T("tri", {freq:"BPM64 L8", mul:0.8}).kr()}, arp),
+    T("pan", {pos:T("lpf", {cutoff:cutoff, Q:10}, VCO).kr()}, arp)
   ).play();
 
-  T("interval", {interval: msec}, function(count) {
+  T("interval", {interval:"BPM128 L16"}, function(count) {
     var i = count % P1.length;
     if (i === 0) CYM.bang();
 
     P1[i].forEach(function(p) { p.bang(); });
 
     if (Math.random() < 0.015) {
-      var j = (Math.random() * P1.length + Math.random() / 5 ) | 0;
+      var j = (Math.random() * P1.length)|0;
       P1.wrapSwap(i, j);
       P2.wrapSwap(i, j);
     }
