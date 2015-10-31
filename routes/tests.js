f59ad77f236fcc455fbaf225309c9c0ad1d15d1d
AlanTai <rainman.tai@gmail.com>
2015-10-31 13:44:02 -0700

    update code

--- routes/tests.js
+++ routes/tests.js
@@ -38,16 +38,6 @@ db.once('open', function() {
 	});
 // end of db
 
-// geoip
-var geoip = require("geoip");
-
-// ipv4 & ipv6 objs for country
-var ipv4_country = new geoip.Country('../my_geoip/GeoIP.dat');
-var ipv6_country = new geoip.Country('../my_geoip/GeoIPv6.dat');
-
-// ipv4 & ipv6 objs for city
-var ipv4_geoip_city = new geoip.City('../my_geoip/GeoLiteCity.dat');
-var ipv6_geoip_city = new geoip.City6('../my_geoip/GeoLiteCityv6.dat');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
